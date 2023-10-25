e8c664e4e0b9220510548688a0e74014e6b66ad6
stephane <stephane.xavier@eleve.ensai.fr>
2023-10-25 16:36:11 +0200

    ok

--- utilisateurs.py
+++
@@ -1,7 +0,0 @@
-class utilisateurs:
-    def__init__(self, id_utilisateurs, nom, mot_de_passe, preferences):
-    self._id_utilisateurs=id_utilisateurs
-    self._nom=nom
-    self._mot_de_passe=mot_de_passe
-    self._preferences=preferences
-    
