3cd88c453c78902bda379447762e142e0c25d4c7
Nathan <nathan.saraivab@gmail.com>
2023-10-26 01:55:25 +0200

    classe

---
+++ Horaire.py
@@ -0,0 +1,5 @@
+class Horaires:
+    def __init__(self,ouverture, fermeture):
+        self.ouverture = ouverture
+        self.fermeture = fermeture
+
---
+++ Station.py
@@ -0,0 +1,19 @@
+class Station:
+    def __init__(
+        self,
+        id_station,
+        nom_station,
+        longitude,
+        latitude,
+        ville,
+        adresse
+    ):
+        self.id_station = id_station
+        self.nom_station = nom_station
+        self.longitude = longitude
+        self.latitude = latitude
+        self.ville = ville
+        self.adresse = adresse
+
+def rechercher_par_preference(self):
+    
