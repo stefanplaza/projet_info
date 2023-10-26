9714a32ca8d8606f0dfeca0e3ac4c52c10d6f10a
Mohamed <mohamed.boukrani@eleve.ensai.fr>
2023-10-26 15:31:01 +0200

    Edit station

--- Classe/Station.py
+++ Classe/Station.py
@@ -14,6 +14,19 @@ class Station:
         self.latitude = latitude
         self.ville = ville
         self.adresse = adresse
+        self.services = []
+
+    def ajouter_service(self, nom_service):
+        self.service.append(nom_service)
+
+    def service_disponible(self, nom_service: str) -> bool:
+        service = True
+
+        if nom_service not in self.service:
+            service = False
+            return service
+
+        return service
 
     def rechercher_par_preference(self):
         pass
