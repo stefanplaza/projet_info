bca15707fb5445edeb6678190f7061739e35ca4d
Nathan <nathan.saraivab@gmail.com>
2023-10-26 16:07:43 +0200

    Creation dossier test et deplacement

--- Horaire.py
+++
@@ -1,5 +0,0 @@
-class Horaires:
-    def __init__(self,ouverture, fermeture):
-        self.ouverture = ouverture
-        self.fermeture = fermeture
-
--- Stations_favorites.py
+++
@@ -1,8 +0,0 @@
-class Station:
-    def __init__(
-        self,
-        id_utilisateur,
-        id_station,
-    ):
-        self._id_utilisateur = id_utilisateur
-        self.id_station = id_station
