1955e4173b48656cf25e5bf63fd4736ed20da680
Nathan <nathan.saraivab@gmail.com>
2023-10-26 14:41:11 +0200

    Test compare

--- Classe/Authentification.py
+++ Classe/Authentification.py
@@ -1,3 +1,5 @@
+import unittest
+
 class Authentification:
     def __init__(
         self,
@@ -22,3 +24,23 @@ class Authentification:
             return True
         else:
             return False
+        
+class TestAuthentification(unittest.TestCase):
+    def setUp(self):
+        # Crée une instance de Authentification pour les tests
+        self.auth = Authentification("utilisateur1", "123")
+
+    def test_compare_identifiants_valides(self):
+        resultat = self.auth.compare("utilisateur1", "123")
+        self.assertTrue(resultat)
+
+    def test_compare_identifiants_invalides(self):
+        resultat = self.auth.compare("utilisateur2", "123")
+        self.assertFalse(resultat)
+
+    def test_compare_mauvais_mot_de_passe(self):
+        resultat = self.auth.compare("utilisateur2", "456")
+        self.assertFalse(resultat)
+
+if __name__ == '__main__':
+    unittest.main()
