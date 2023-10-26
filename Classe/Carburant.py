e6237b2a22861e3d541f46937770127f47e6c516
Mathis <mathis.bouillon@eleve.ensai.fr>
2023-10-26 11:58:44 +0200

    possibilité modif classe

--- Classe/Carburant.py
+++ Classe/Carburant.py
@@ -2,3 +2,5 @@ class Carburant:
     def __init__(self, id_carburant, nom_carburant):
         self.id_carburant = id_carburant
         self.nom_carburant = nom_carburant
+#il faudra rajouter une classe PrixCarburant je pense (les prix changent tout le temps donc galère)
+#cette classe pourait donc devenir TypeCarburant()
