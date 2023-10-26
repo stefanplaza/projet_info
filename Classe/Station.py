f6e16567b94a75c6967e851e9598afc0bb061c0e
Mathis <mathis.bouillon@eleve.ensai.fr>
2023-10-26 16:50:38 +0200

    changement attribut classe station

--- Classe/Station.py
+++ Classe/Station.py
@@ -5,13 +5,21 @@ class Station:
         longitude,
         latitude,
         ville,
-        adresse
+        adresse,
+        services, 
+        type_carburant,
+        prix_carburant, 
+        coordonnees,
     ):
         self.id_station = id_station
         self.longitude = longitude
         self.latitude = latitude
         self.ville = ville
         self.adresse = adresse
+        self.services = services 
+        self.type_carburant = type_carburant, 
+        self.prix_carburant = prix_carburant, 
+        self.coordonnees = coordonnees
 
     def rechercher_par_preference(self):
         pass
