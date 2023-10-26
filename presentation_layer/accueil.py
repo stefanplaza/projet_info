daf7fe8a77a26aeeece9482f08cfc9e6f1d7ea5d
Quentin <id2315@DOMENSAI.ECOLE>
2023-10-26 17:06:28 +0200

    biiiiii

--- presentation_layer/accueil.py
+++ presentation_layer/accueil.py
@@ -4,3 +4,16 @@ print("Quelles sont les services que vous désirez ?")
 y = input()
 print("Quel est le type de caarburant souhaité ?")
 z = input()
+
+from abc import ABC, abstractmethod
+
+class AbstractView(ABC):
+    def display_info(self):
+        return None
+
+    @abstractmethod
+    def make_choice(self):
+        pass
+
+class Connexion_View(AbstractView):
+    
