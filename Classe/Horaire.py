77684dd29eaea614569d922a0b5ac86233dac08e
stephane <stephane.xavier@eleve.ensai.fr>
2023-10-27 14:46:57 +0200

    horaire

--- Classe/Horaire.py
+++ Classe/Horaire.py
@@ -1,5 +1,11 @@
 class Horaires:
-    def __init__(self,ouverture, fermeture):
+    def __init__(self, ouverture, fermeture):
+        """
+        Initialise une instance de la classe Horaires avec des horaires d'ouverture et de fermeture.
+
+        Args:
+            ouverture (str): L'heure d'ouverture au format HH:MM (par exemple, "08:00").
+            fermeture (str): L'heure de fermeture au format HH:MM (par exemple, "18:00").
+        """
         self.ouverture = ouverture
         self.fermeture = fermeture
-
