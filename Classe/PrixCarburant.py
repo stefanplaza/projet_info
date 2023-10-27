14bd078627ec6af9c6d9e5e55ee5ec084857b4b6
stephane <stephane.xavier@eleve.ensai.fr>
2023-10-27 14:54:44 +0200

    doc carburant

--- Classe/PrixCarburant.py
+++ Classe/PrixCarburant.py
@@ -1,6 +1,13 @@
 from Classe.TypeCarburant import TypeCarburant
 
 class PrixCarburant:
-    def __init__(self, id_carburant,prix):
+    def __init__(self, id_carburant, prix):
+        """
+        Initialise une instance de la classe PrixCarburant avec un identifiant de carburant et son prix.
+
+        Args:
+            id_carburant (int): L'identifiant du type de carburant associé à l'instance.
+            prix (float): Le prix du carburant en unité monétaire (par exemple, en euros par litre).
+        """
         self.id_carburant = id_carburant
         self.prix = prix
