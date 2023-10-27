b7cae061c632e557a836bcb915b37790e76de4f1
stephane <stephane.xavier@eleve.ensai.fr>
2023-10-27 16:30:49 +0200

    convertion

---
+++ API/convertion.py
@@ -0,0 +1,17 @@
+from geopy.geocoders import Nominatim
+
+
+def adresse_en_coordonnees(adresse):
+    geolocator = Nominatim(user_agent="géoloc")  # Remplacez "myGeocoder" par votre propre nom d'utilisateur.
+
+    location = geolocator.geocode(adresse)
+
+    if location:
+        latitude = location.latitude
+        longitude = location.longitude
+        return latitude, longitude
+    else:
+        return None
+
+adresse_en_coordonnees("rue jules vernes, Langueux")
+
