5e036f1ff703095d089b2970ccb8c9703cd759f0
stephane <stephane.xavier@eleve.ensai.fr>
2023-10-27 15:13:04 +0200

    doc utilisateur

--- Classe/Utilisateur.py
+++ Classe/Utilisateur.py
@@ -5,7 +5,22 @@ class Utilisateur:
         nom_utilisateur,
         mot_de_passe,
     ):
+        """
+        Initialise une instance de la classe Utilisateur avec un identifiant d'utilisateur, un nom d'utilisateur et un mot de passe.
+
+        Args:
+            id_utilisateur (int): L'identifiant unique de l'utilisateur associé à l'instance.
+            nom_utilisateur (str): Le nom d'utilisateur ou le pseudonyme de l'utilisateur.
+            mot_de_passe (str): Le mot de passe de l'utilisateur pour l'authentification.
+
+        Attributes:
+            _id_utilisateur (int): L'identifiant unique de l'utilisateur.
+            _nom_utilisateur (str): Le nom d'utilisateur ou pseudonyme.
+            _mot_de_passe (str): Le mot de passe de l'utilisateur.
+
+        Example:
+            utilisateur = Utilisateur(1, "john_doe", "mot_de_passe_secure")
+        """
         self._id_utilisateur = id_utilisateur
         self._nom_utilisateur = nom_utilisateur
         self._mot_de_passe = mot_de_passe
-
