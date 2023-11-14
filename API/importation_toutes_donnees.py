a715e72818842427a6d22e437ec78ff2cdcc2f5c
Mathis <mathis.bouillon@eleve.ensai.fr>
2023-11-14 22:17:53 +0100

    modif classe coordonnees  et api

--- API/importation_toutes_donnees.py
+++ API/importation_toutes_donnees.py
@@ -1,8 +1,8 @@
-
 import requests
 import zipfile
 import io
 import xml.etree.ElementTree as ET
+from Classe.Coordonnees import Coordonnees
 
 url = "https://donnees.roulez-eco.fr/opendata/instantane"
 
@@ -23,27 +23,69 @@ try:
                 ids = []
                 latitude = []
                 longitude = []
-                cp=[]
+                cp = []
+                # Services à rechercher
+                services_recherches = ["Station de gonflage"]
+                carburants_recherches = ["Gazole"]
 
 
                 # Parcourez tous les éléments <pdv> dans le XML
                 for pdv_element in root.findall(".//pdv"):
-                    pdv_id = pdv_element.get("id")
-                    pdv_latitude = pdv_element.get("latitude")
-                    pdv_longitude = pdv_element.get("longitude")
-                    pdv_cp = pdv_element.get("cp")
-                    ids.append(pdv_id)
-                    latitude.append(pdv_latitude)
-                    longitude.append(pdv_longitude)
-                    cp.append(pdv_cp)
+                    # Obtenez la liste des services pour la station
+                    services = [service.text for service in pdv_element.findall(".//services/service")]
+                    carburants = [carburant_element.get("nom") for carburant_element in pdv_element.findall(".//prix")]
 
-                print(ids)
-                #print(latitude)
-                #print(longitude)
-                #print(cp)
+
+                    # Vérifiez si les services recherchés sont présents dans la liste des services
+                    if all(service in services for service in services_recherches):
+                        if any(carburant in carburants_recherches for carburant in carburants):
+                            pdv_id = pdv_element.get("id")
+                            pdv_latitude = pdv_element.get("latitude")
+                            pdv_longitude = pdv_element.get("longitude")
+                            pdv_cp = pdv_element.get("cp")
+                            ids.append(pdv_id)
+                            latitude.append(pdv_latitude)
+                            longitude.append(pdv_longitude)
+                            cp.append(pdv_cp)
                
-            else:
-                print(f"Le fichier {xml_file_name} n'existe pas dans le fichier zip.")
+                #print(len(longitude))
+                #print(len(ids))
+                coor_utilisateur = Coordonnees(0,48.6428477,2.7143162)
+                coor_info = [Coordonnees(pdv_id, float(pdv_latitude)/100000, float(pdv_longitude)/100000) for pdv_id, pdv_latitude, pdv_longitude in zip(ids, latitude, longitude)]
+                #print(coor_info)
+
+                dist=[]
+                for x in coor_info:
+                    y= x.dist(coor_utilisateur)
+                    dist.append(y)
+                #print(dist)
+               
+                print(len(coor_info))
+
+                def trier(liste : list[list]):
+                    liste_triee = sorted(liste, key=lambda x: x[1])
+                    return liste_triee
+
+                    print(liste_triee)
+               
+                dist_triee = trier(dist)
+
+                longueur = 40
+                ligne_de_pointilles = "-" * longueur
+                print(ligne_de_pointilles)
+
+                #print(dist_triee)
+
+                def selectionner_cinq_premiers (liste : list[list]) :
+                    l=[]
+                    for i in range(5):
+                        l.append(liste[i])
+                    return l
+               
+                print(ligne_de_pointilles)
+
+                dist_triee_premiers = selectionner_cinq_premiers(dist_triee)
+                print(dist_triee_premiers)
 
     else:
         print("La requête a échoué avec le code de statut :", response.status_code)
--- Classe/Coordonnees.py
+++ Classe/Coordonnees.py
@@ -1,53 +1,37 @@
+import unittest
 import math
 
 class Coordonnees:
-    def __init__(self, latitude, longitude):
-        """
-        Initialise une instance de la classe Coordonnees avec des coordonnées de latitude et de longitude.
-    
-        Args:
-            latitude (float): La latitude en degrés.
-            longitude (float): La longitude en degrés.
-        """
+    def __init__(self, id_station, latitude, longitude):
         self.latitude = latitude
         self.longitude = longitude
-
-    def __str__(self):
-        return f"ID utilisateur: {self._id_utilisateur}, mot de passe : {self._mot_de_passe}"
-
-         """
-        Retourne une représentation sous forme de chaîne de caractères de l'objet Utilisateur.
-
-        Returns:
-            str: Une chaîne de caractères représentant l'ID de l'utilisateur et son mot de passe.
-        """     
+        self.id_station = id_station
+   
+    def __str__(self) -> str:
+        return f"la station {self.id_station} a pour coordonnees (latitude={self.latitude}, longitude={self.longitude})"
 
     def dist(self, autre):
-        """
-        Calcule la distance entre deux points géographiques en utilisant la formule de la haversine.
-
-        Args:
-            autre (Coordonnees): Les coordonnées de l'autre point géographique.
-
-        Returns:
-            float: La distance en kilomètres entre les deux points géographiques.
-        """
         # Convertir les latitudes et longitudes de degrés à radians
         lat1, lon1 = math.radians(self.latitude), math.radians(self.longitude)
         lat2, lon2 = math.radians(autre.latitude), math.radians(autre.longitude)
-        
+       
         # Calcul des différences de latitude et de longitude
         dlat = lat2 - lat1
         dlon = lon2 - lon1
-        
+       
         # Appliquer la formule haversine
         a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-        
+       
         # Rayon de la Terre en kilomètres (par défaut)
         R = 6371.0  # En kilomètres
-        
+       
         # Calculer la distance
         distance = R * c
-        
-        return distance
+       
+        return [self.id_station ,distance]
+   
+#####test
+coor = Coordonnees(562,62,6)
+coor2= Coordonnees(8498,62.1,6)
+print(coor.dist(coor2))
