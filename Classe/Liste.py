f15cb514c4656c107211e6fed9ffff07cad255bc
Nathan <nathan.saraivab@gmail.com>
2023-11-17 10:05:22 +0100

    maj classe metier

--- Classe/Liste.py
+++ Classe/Liste.py
@@ -7,5 +7,5 @@ class Liste:
 def __str__(self):
     return f"Liste(id_liste={self.id_liste}, id_utilisateur={self.id_utilisateur}, nom_liste='{self.nom_liste}')"
 
-ma_liste = Liste(1, 101, 'MaListe')
-print(ma_liste)
+# ma_liste = Liste(1, 101, 'MaListe')
+# print(ma_liste)
