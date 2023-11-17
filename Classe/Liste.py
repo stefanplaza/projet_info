8de0f4ee0741c3273320aac37d47a58b9e6ef081
Nathan <nathan.saraivab@gmail.com>
2023-11-17 09:57:38 +0100

    maj classe metier

--- Classe/Liste.py
+++ Classe/Liste.py
@@ -3,3 +3,9 @@ class Liste:
         self.id_liste = id_liste
         self.id_utilisateur = id_utilisateur
         self.nom_liste = nom_liste
+
+def __str__(self):
+    return f"Liste(id_liste={self.id_liste}, id_utilisateur={self.id_utilisateur}, nom_liste='{self.nom_liste}')"
+
+ma_liste = Liste(1, 101, 'MaListe')
+print(ma_liste)
